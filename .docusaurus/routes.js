2adfb61d30b935bbea841f6e06647fd1852681eb
happydhKim <happytime870616@gmail.com>
2020-06-26 22:21:50 +0900

    [FIX] Settings

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -5,91 +5,91 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 export default [
   
 {
-  path: '/dropy-online/dropy.tech/',
-  component: ComponentCreator('/dropy-online/dropy.tech/'),
+  path: '/dropy-tech/',
+  component: ComponentCreator('/dropy-tech/'),
   exact: true,
   
 },
 {
-  path: '/dropy-online/dropy.tech/blog',
-  component: ComponentCreator('/dropy-online/dropy.tech/blog'),
+  path: '/dropy-tech/blog',
+  component: ComponentCreator('/dropy-tech/blog'),
   exact: true,
   
 },
 {
-  path: '/dropy-online/dropy.tech/blog/hello-world',
-  component: ComponentCreator('/dropy-online/dropy.tech/blog/hello-world'),
+  path: '/dropy-tech/blog/hello-world',
+  component: ComponentCreator('/dropy-tech/blog/hello-world'),
   exact: true,
   
 },
 {
-  path: '/dropy-online/dropy.tech/blog/hola',
-  component: ComponentCreator('/dropy-online/dropy.tech/blog/hola'),
+  path: '/dropy-tech/blog/hola',
+  component: ComponentCreator('/dropy-tech/blog/hola'),
   exact: true,
   
 },
 {
-  path: '/dropy-online/dropy.tech/blog/tags',
-  component: ComponentCreator('/dropy-online/dropy.tech/blog/tags'),
+  path: '/dropy-tech/blog/tags',
+  component: ComponentCreator('/dropy-tech/blog/tags'),
   exact: true,
   
 },
 {
-  path: '/dropy-online/dropy.tech/blog/tags/docusaurus',
-  component: ComponentCreator('/dropy-online/dropy.tech/blog/tags/docusaurus'),
+  path: '/dropy-tech/blog/tags/docusaurus',
+  component: ComponentCreator('/dropy-tech/blog/tags/docusaurus'),
   exact: true,
   
 },
 {
-  path: '/dropy-online/dropy.tech/blog/tags/facebook',
-  component: ComponentCreator('/dropy-online/dropy.tech/blog/tags/facebook'),
+  path: '/dropy-tech/blog/tags/facebook',
+  component: ComponentCreator('/dropy-tech/blog/tags/facebook'),
   exact: true,
   
 },
 {
-  path: '/dropy-online/dropy.tech/blog/tags/hello',
-  component: ComponentCreator('/dropy-online/dropy.tech/blog/tags/hello'),
+  path: '/dropy-tech/blog/tags/hello',
+  component: ComponentCreator('/dropy-tech/blog/tags/hello'),
   exact: true,
   
 },
 {
-  path: '/dropy-online/dropy.tech/blog/tags/hola',
-  component: ComponentCreator('/dropy-online/dropy.tech/blog/tags/hola'),
+  path: '/dropy-tech/blog/tags/hola',
+  component: ComponentCreator('/dropy-tech/blog/tags/hola'),
   exact: true,
   
 },
 {
-  path: '/dropy-online/dropy.tech/blog/welcome',
-  component: ComponentCreator('/dropy-online/dropy.tech/blog/welcome'),
+  path: '/dropy-tech/blog/welcome',
+  component: ComponentCreator('/dropy-tech/blog/welcome'),
   exact: true,
   
 },
 {
-  path: '/dropy-online/dropy.tech/docs',
-  component: ComponentCreator('/dropy-online/dropy.tech/docs'),
+  path: '/dropy-tech/docs',
+  component: ComponentCreator('/dropy-tech/docs'),
   
   routes: [
 {
-  path: '/dropy-online/dropy.tech/docs/',
-  component: ComponentCreator('/dropy-online/dropy.tech/docs/'),
+  path: '/dropy-tech/docs/',
+  component: ComponentCreator('/dropy-tech/docs/'),
   exact: true,
   
 },
 {
-  path: '/dropy-online/dropy.tech/docs/doc2',
-  component: ComponentCreator('/dropy-online/dropy.tech/docs/doc2'),
+  path: '/dropy-tech/docs/doc2',
+  component: ComponentCreator('/dropy-tech/docs/doc2'),
   exact: true,
   
 },
 {
-  path: '/dropy-online/dropy.tech/docs/doc3',
-  component: ComponentCreator('/dropy-online/dropy.tech/docs/doc3'),
+  path: '/dropy-tech/docs/doc3',
+  component: ComponentCreator('/dropy-tech/docs/doc3'),
   exact: true,
   
 },
 {
-  path: '/dropy-online/dropy.tech/docs/mdx',
-  component: ComponentCreator('/dropy-online/dropy.tech/docs/mdx'),
+  path: '/dropy-tech/docs/mdx',
+  component: ComponentCreator('/dropy-tech/docs/mdx'),
   exact: true,
   
 }],
